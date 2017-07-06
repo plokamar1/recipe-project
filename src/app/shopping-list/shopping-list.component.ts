9d92ed9814b5e3de22847abbb768541cd14d2d85
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-07-06 10:41:13 +0300

    Starting the recipe list component

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  shoppingList = [];
+  newProduct = '';
+  constructor() { }
+
+  ngOnInit() {
+  }
+  addNewProduct(){
+    this.shoppingList.push(this.newProduct);
+  }
+}
