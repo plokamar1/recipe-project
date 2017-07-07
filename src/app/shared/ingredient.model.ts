79c9c030efc2d809fb72d63bb1f32ec32a32ebc8
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-07-07 11:29:39 +0300

    Made the ingredient model

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,13 @@
+export class Ingredient {
+/*  public name: string;
+  public amount: number;
+
+  constructor(name: string, amount: number) {
+    this.name = name;
+    this.amount = amount;
+
+Typescript offers a faster approach of declaration of variables like this
+  }*/
+
+  constructor(public name: string, public amount: number) {}
+}
