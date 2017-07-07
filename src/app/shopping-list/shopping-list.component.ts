ec42b4267d54008ea45f47fedf81d24623cd467b
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-07-07 12:27:00 +0300

    I am using the Ingredient model i made before to produce a list with ngFor

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model'
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+/*
   shoppingList = [];
   newProduct = null;
   isEmpty = true;
+*/
+  ingredients: Ingredient[] = [
+    new Ingredient('Apple', 5),
+    new Ingredient('Tomato', 4)
+  ];
+
 
   constructor() {
 
@@ -16,10 +24,10 @@ export class ShoppingListComponent implements OnInit {
 
   ngOnInit() {
   }
-  checkEmpty() {
+/*  checkEmpty() {
     this.newProduct === null ? this.isEmpty = true : this.isEmpty = false;
   }
   addNewProduct() {
     this.shoppingList.push(this.newProduct);
-  }
+  }*/
 }
