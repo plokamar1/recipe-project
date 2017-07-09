5828841baa6891c2ebddedc208b2aceebd7262a5
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-07-09 18:54:48 +0300

    The parent component receives the recipe object and sends it to the recipe- detail component so the information will be shown. In the html file we bind the recipe object we received in the recipe variable waiting for input in the recipe-detail component.

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from './recipe.model'
+
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
-
+  recipeModel: Recipe ;
   constructor() { }
 
   ngOnInit() {
   }
 
+  onRecipeSelect(recipeData: Recipe) {
+    this.recipeModel = recipeData;
+  console.log(this.recipeModel);
+  }
 }
