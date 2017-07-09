faf8c5acb74706226ef81a012d64a77228c2969e
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-07-09 18:51:15 +0300

    Made an event emiter from which we send the selected recipe object to the parent component.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 import { Recipe } from '../recipe.model'
 
@@ -10,12 +10,18 @@ import { Recipe } from '../recipe.model'
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png'),
-    new Recipe('A Test Recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png')
+    new Recipe('Another test recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png')
   ];
+
+  @Output() selectedRecipe = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  emitRecipeData( selectedRecipe: Recipe ){
+    this.selectedRecipe.emit(selectedRecipe);
+    console.log('Recipe emitted');
+  }
 }
