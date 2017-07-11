84f5d8cd6a0b72d0ece1673056a893468d71ad49
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-07-11 14:55:48 +0300

    Without using Routing i made a simple component selector. So the components are shown according to the selection of the user from the header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  selectedComponent = 0;
   title = 'app';
+
+  changeComponent(component: number) {
+    this.selectedComponent = component;
+  }
 }
