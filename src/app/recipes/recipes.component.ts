415767b5626dfbccd8a73a5ffe3dcf3e2a4ddc9f
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-07-19 12:25:54 +0300

    Added the service to a different location and provided the service in the parent recipes.component

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from './recipe.model'
+import {RecipeService} from "./recipe.service";
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css']
+  styleUrls: ['./recipes.component.css'],
+  providers: [ RecipeService ]
 })
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe ;
@@ -14,8 +16,4 @@ export class RecipesComponent implements OnInit {
   ngOnInit() {
   }
 
-  onRecipeSelect(recipeData: Recipe) {
-    this.selectedRecipe = recipeData;
-  //console.log(this.recipeModel);
-  }
 }
