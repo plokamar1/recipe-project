651a2437d06e5eba9d8110888c39268ff20497af
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-07-21 14:01:52 +0300

    Now every recipe has it's ingredients. They are also shown in the recipe page in the same way they are shown in the shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,23 @@
 import { Recipe } from "./recipe.model"
 import {EventEmitter} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipeService {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another test recipe', 'This is a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe('A Test Recipe',
+      'This is a test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [new Ingredient('tomato', 4),
+      new Ingredient('potato', 2),
+      new Ingredient('Cumin', 3)]
+      ),
+    new Recipe('Another test recipe',
+      'This is a test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [new Ingredient('watermelon', 4),
+        new Ingredient('salt', 2),
+        new Ingredient('vodka', 3)]
+      )
   ];
 
   selectedRecipe = new EventEmitter<Recipe>();
